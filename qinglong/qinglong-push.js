2ff549579e63b51e005a83499cbef333310c86b1
cym_lab <113275185+tiandaon123@users.noreply.github.com>
2026-03-06 17:49:56 +0800

    qinglong-push.js

--- qinglong/qinglong-push.js
+++ qinglong/qinglong-push.js
@@ -913,6 +913,8 @@ const dataAggregationService = {
 
       // 基础信息
       data.date = { value: dayjs().format('YYYY年MM月DD日') }
+      data.date.value = `兔子早安，开启活力一天！记得多喝水、忌久坐，注意休息你的大眼，以及适当运动。\n${data.date.value}`
+      
       // 可配置：自定义字段（从 user.customFields 注入到模板 data）
       if (user.customFields && typeof user.customFields === 'object') {
         for (const [k, v] of Object.entries(user.customFields)) {
